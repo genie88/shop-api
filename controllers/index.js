42effad28b6467e097b0d172ea2e2a2ecc4cb697
genie <genie88@163.com>
2015-12-19 13:26:45 +0800

    修改链接

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,5 @@ var rendering = require('../util/rendering');
 
 
 exports.home = function(req, res) {
-    res.render('index/index');
-}
-
-
-exports.userHome = function(req, res) {
-    res.render('index/user-home');
+    res.render('home/index');
 }
--- routes.js
+++ routes.js
@@ -6,8 +6,8 @@ var rendering = require('./util/rendering'),
 module.exports = function (app, passport) {
 
     // Home
-    app.get('/', indexController.home);
-    app.get('/home', ensureAuthenticated, indexController.userHome);
+    app.get('/', ensureAuthenticated, indexController.home);
+    app.get('/home', ensureAuthenticated, indexController.home);
 
 
     // Auth
@@ -16,23 +16,6 @@ module.exports = function (app, passport) {
     app.get('/login', loginController.loginPage);
     app.post('/login', loginController.checkLogin);
     app.get('/logout', loginController.logout);
-
-    // 'rendering' can be used to format api calls (if you have an api)
-    // into either html or json depending on the 'Accept' request header
-    app.get('/apitest', function(req, res) {
-        rendering.render(req, res, {
-            'data': {
-                'test': {
-                    'testsub': {
-                        'str': 'testsub hello world'
-                    },
-                    'testsub2': 42
-                },
-                'test2': 'hello world'
-            }
-        });
-    })
-
 
     // Auth Middleware
     function ensureAuthenticated(req, res, next) {
