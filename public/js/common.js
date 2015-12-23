eabec5ca7b66c89711f74cf2aad452a63c2a078b
genie <genie88@163.com>
2015-12-23 12:42:40 +0800

    完善首页requirejs 配置

--- public/js/common.js
+++ public/js/common.js
@@ -5,7 +5,17 @@ requirejs.config({
         modernizr: 'modernizr.min',
         'jquery-ui': 'jquery-ui-1.9.2.custom.min',
         'nicescroll': 'jquery.nicescroll',
-        'easypiechart': 'easypiechart/jquery.easypiechart'
+        'easypiechart': 'easypiechart/jquery.easypiechart',
+        'iCheck': 'iCheck/jquery.icheck',
+        'sparkline': 'sparkline/jquery.sparkline',
+        'flot-chart': 'flot-chart/jquery.flot',
+        'flot-tooltip':'flot-chart/jquery.flot.tooltip',
+        'flot-resize': 'flot-chart/jquery.flot.resize',
+        'morris': 'morris-chart/morris',
+        'raphael': 'morris-chart/raphael-min',
+        'calendar': 'calendar/clndr',
+        'moment': 'calendar/moment-2.2.1'
+
     },
     shim: {
         "bootstrap": ["jquery"],
@@ -16,9 +26,39 @@ requirejs.config({
             deps: ['jquery'],
             exports: '$.fn.niceScroll'
         },
-        easypiechart:{
+        // easypiechart:{
+        //     deps: ['jquery'],
+        //     exports: '$.fn.easyPieChart'
+        // },
+        iCheck: {
             deps: ['jquery'],
-            exports: '$.fn.easyPieChart'
+            exports: '$.fn.iCheck'
+        },
+        sparkline: {
+            deps: ['jquery'],
+            exports: '$.fn.sparkline'
+        },
+        'flot-chart': {
+            deps: ['jquery'],
+            exports: '$.plot'
+        },
+        'flot-tooltip': {
+            deps: ['jquery', 'flot-chart']
+        },
+        'flot-resize': {
+            deps: ['jquery', 'flot-chart']
+        },
+        'raphael': {
+            deps: [],
+            exports: 'Raphael'
+        },
+        'morris': {
+            deps: ['jquery', 'raphael'],
+            exports: 'Morris'
+        },
+        'calendar': {
+            deps: ['jquery', 'moment', 'underscore'],
+            exports: '$.fn.clndr'
         }
     }
 });
