4193b88fbdcdfd6391767717a8ab42dd99b41461
genie <genie88@163.com>
2016-01-13 11:25:13 +0800

    增加模块字段定义

--- public/js/app/modules/index.js
+++ public/js/app/modules/index.js
@@ -1,8 +1,8 @@
-define(['jquery', 'swig', 'ckeditor', 'app/pager', 'fileupload', 'comp/dialog/index', 'app/base', 'api/index', ], 
+define(['jquery', 'swig', 'ckeditor', 'app/pager', 'fileupload', 'comp/dialog/index', 'app/base', 'api/index'], 
     function ($, swig, CKEDITOR,Pager, upload, Dialog, BaseController, API) {
     var jQuery = $;
     //console.log(CKEDITOR)
-    var moduleId = window.location.pathname.split('/')[2];
+    var moduleId = parseInt(window.location.pathname.split('/')[2]);
     window.api = new API('http://localhost:3000/api', {});
 
 
@@ -18,7 +18,32 @@ define(['jquery', 'swig', 'ckeditor', 'app/pager', 'fileupload', 'comp/dialog/in
     _p.initDetail = function(){
         var self = this;
         self.pager = new Pager({wrapper: $('.pagination ul'), total: 8, page: 2});
-        self.getFragments({}, {page: 1, page_size: 5});
+
+        //获取模块字段含义
+        api.modules.get(moduleId, {}, function(json){
+            if(json && json.code == 200 && json.data && json.data) {
+                self.$scope.module = json.data;
+                //console.log(self.$scope.module.ext1);
+
+                //mock
+                //支持的类型包括 date/color/text/mtext/image/number/link/
+                self.$scope.module.ext1 = [
+                    {key: 'title', label:'标题', type: 'text', validator: {}},
+                    {key: 'subtitle', label:'图片', type: 'image', validator: {}},
+                    {key: 'abstract', label:'简介', type: 'mtext', validator: {}},
+                    {key: 'content', label:'正文', type: 'mtext', validator: {}},
+                    {key: 'sort', label:'排序', type: 'number', validator: {}},
+                    {key: 'is_show', label:'显示/隐藏', type: 'checkbox', validator: {}},
+                    {key: 'ext1', label:'跳转链接', type: 'alink', validator: {}},
+                    {key: 'ext2', label:'浏览次数', type: 'number', validator: {}}
+                ]
+
+                self.getFragments({}, {page: 1, page_size: 5});
+                self.apply();
+            }
+        })
+
+        
         $(document).on('PAGER_CHANGED', function(e, page){
             self.getFragments({}, {page: page, page_size: 5});
         })
@@ -27,7 +52,7 @@ define(['jquery', 'swig', 'ckeditor', 'app/pager', 'fileupload', 'comp/dialog/in
     _p.getFragments = function(query, filter){
         var self = this;
         api.modules.fragments(moduleId).list({}, function(json){
-            console.log(json.data)
+            //console.log(json.data)
             if(json && json.code == 200 && json.data) {
                 self.$scope.fragments = json.data.data;
                 self.page = json.data.currentPage;
